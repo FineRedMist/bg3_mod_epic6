8d9d7ef81cfe183f4ec0fe0985d637471ae67b99
Brent Scriver <bscriver@oneoddsock.com>
2024-08-04 13:19:59 -0400

    Experimenting with the window.

--- DnD-Epic6/Mods/DnD-Epic6/ScriptExtender/Lua/Client/E6_NetClientHandlers.lua
+++ DnD-Epic6/Mods/DnD-Epic6/ScriptExtender/Lua/Client/E6_NetClientHandlers.lua
@@ -7,6 +7,18 @@ function NetClientHandlers.ShowFeatSelectorUI(_, payload, peerId)
         _E6Error("Failed to get entity for player id: " .. payload)
         return
     end
+
+    local featTitle = Ext.Loca.GetTranslatedString("h1a5184cdgaba1g432fga0d3g51ac15b8a0a8")
+    local win = Ext.IMGUI.NewWindow(featTitle)
+    win.Closeable = true
+    win.AlwaysAutoResize = true
+    win:AddText("Select a feat to learn for: " .. ent.CharacterCreationStats.Name)
+    local button = win:AddButton("Close")
+    button.OnClick = function()
+       win:Destroy()
+    end
+    win:SetFocus()
+
     --if userCharacter then
     --    local MCMModVars = Ext.Vars.GetModVariables(ModuleUUID)
     --    if updateNotificationStatus(userId, MCMModVars) then
