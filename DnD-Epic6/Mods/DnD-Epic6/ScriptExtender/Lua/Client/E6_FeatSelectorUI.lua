e7208790072d36f5684816fe269a7ca49e571d4a
Brent Scriver <bscriver@oneoddsock.com>
2024-08-11 12:57:11 -0400

    Working on the feat detail panel. Lots of complications. No word wrap or figuring out centering

--- DnD-Epic6/Mods/DnD-Epic6/ScriptExtender/Lua/Client/E6_FeatSelectorUI.lua
+++ DnD-Epic6/Mods/DnD-Epic6/ScriptExtender/Lua/Client/E6_FeatSelectorUI.lua
@@ -1,39 +1,63 @@
 
 ---@type ExtuiWindow?
-local win = nil
+local featUI = nil
+local featDetailUI = nil
 
 local function CalculateLayout()
     _E6P("Client UI Layout value: " .. tostring(Ext.ClientUI.GetStateMachine().State.Layout))
 end
 
-local function FeatDetailSelectUI()
-    local win = Ext.IMGUI.NewWindow("Feat Detail")
-    win.Closeable = true
-    win.AlwaysAutoResize = true
-    win:SetFocus()
-    win.Visible = true
-    win.Open = true
-    win.UserData = nil
-    return win
-end
----Creates a button in the feat selection window for the feat.
----@param win ExtuiWindow The window to close on completion.
----@param buttonWidth number The width of the button.
----@param playerId string The player id for the feat.
----@param feat table The feat to create the button for.
----@return ExtuiButton The button created.
-local function MakeFeatButton(win, buttonWidth, playerId, feat)
-    local featButton = win:AddButton(feat.DisplayName)
-    featButton.UserData = feat
-    featButton.Size = {buttonWidth-30, 48}
-    featButton:SetStyle("ButtonTextAlign", 0.5, 0.5)
-    featButton.OnClick = function()
-        win.Visible = false
-        win.Open = false
-        win.UserData = nil
+---comment
+---@param feat table The feat to create the window for.
+---@param playerInfo table The player id for the feat.
+local function ShowFeatDetailSelectUI(feat, playerInfo)
+    local windowDimensions = {1000, 1450}
+    if not featDetailUI then
+        local featDetails = Ext.Loca.GetTranslatedString("h43800b7agdc92g46b6g82dcg22fb987efe6c")
+        featDetailUI = Ext.IMGUI.NewWindow(featDetails)
+        featDetailUI.Closeable = true
+        featDetailUI.NoMove = true
+        featDetailUI.NoResize = true
+        featDetailUI.NoCollapse = true
+        featDetailUI:SetSize(windowDimensions)
+        featDetailUI:SetPos({1400, 100})
+    end
+
+    featDetailUI.Visible = true
+    featDetailUI.Open = true
+    featDetailUI:SetFocus()
+
+    local children = featDetailUI.Children
+    for _, child in ipairs(children) do
+        featDetailUI:RemoveChild(child)
+    end
+
+    local childWin = featDetailUI:AddChildWindow("Selection")
+    childWin.Size = {windowDimensions[1], windowDimensions[2] - 150}
+    childWin.PositionOffset = {0, 0}
+    childWin.NoTitleBar = true
+    local description = childWin:AddText(feat.Description)
+    description.ItemWidth = windowDimensions[1] - 100
+    local select = featDetailUI:AddButton(Ext.Loca.GetTranslatedString("h04f38549g65b8g4b72g834eg87ee8863fdc5"))
+   
+    select:SetStyle("ButtonTextAlign", 0.5, 0.5)
+    -- Doesn't work :(
+    select.OnActivate = function()
+        local buttonWidth = select.ItemWidth
+        local offset = select.PositionOffset
+        if offset and buttonWidth then
+            local newX = (windowDimensions[1] - 2 * offset[1] - buttonWidth) / 2
+            select.PositionOffset = {newX, offset[2]}
+        end
+    end
+    select.OnClick = function()
+        featUI.Visible = false
+        featUI.Open = false
+        featDetailUI.Visible = false
+        featDetailUI.Open = false
 
         local payload = {
-            PlayerId = playerId,
+            PlayerId = playerInfo.ID,
             Feat = {
                 FeatId = feat.ID,
                 PassivesAdded = feat.PassivesAdded
@@ -41,6 +65,21 @@ local function MakeFeatButton(win, buttonWidth, playerId, feat)
         }
         local payloadStr = Ext.Json.Stringify(payload)
         Ext.Net.PostMessageToServer(NetChannels.E6_CLIENT_TO_SERVER_SELECTED_FEAT_SPEC, payloadStr)
+    end
+end
+
+---Creates a button in the feat selection window for the feat.
+---@param win ExtuiWindow The window to close on completion.
+---@param buttonWidth number The width of the button.
+---@param playerInfo table The player id for the feat.
+---@param feat table The feat to create the button for.
+---@return ExtuiButton The button created.
+local function MakeFeatButton(win, buttonWidth, playerInfo, feat)
+    local featButton = win:AddButton(feat.DisplayName)
+    featButton.Size = {buttonWidth-30, 48}
+    featButton:SetStyle("ButtonTextAlign", 0.5, 0.5)
+    featButton.OnClick = function()
+        ShowFeatDetailSelectUI(feat, playerInfo)
     end
     return featButton
 end
@@ -50,28 +89,24 @@ function E6_FeatSelectorUI(message)
     local windowDimensions = {500, 1450}
     CalculateLayout()
 
-    if win == nil then
+    if featUI == nil then
         local featTitle = Ext.Loca.GetTranslatedString("h1a5184cdgaba1g432fga0d3g51ac15b8a0a8")
-        win = Ext.IMGUI.NewWindow(featTitle)
-        win.Closeable = true
-        win.NoMove = true
-        win.NoResize = true
-        win.NoCollapse = true
-        win:SetSize(windowDimensions)
-        win:SetPos({800, 100})
+        featUI = Ext.IMGUI.NewWindow(featTitle)
+        featUI.Closeable = true
+        featUI.NoMove = true
+        featUI.NoResize = true
+        featUI.NoCollapse = true
+        featUI:SetSize(windowDimensions)
+        featUI:SetPos({800, 100})
     else
-        win.Visible = true
-        win.Open = true
+        featUI.Visible = true
+        featUI.Open = true
     end
-    win.OnClose = function()
-        win.UserData = nil
-    end
-    win:SetFocus()
+    featUI:SetFocus()
 
-    win.UserData = message
-    local children = win.Children
+    local children = featUI.Children
     for _, child in ipairs(children) do
-        win:RemoveChild(child)
+        featUI:RemoveChild(child)
     end
 
     local allFeats = E6_GatherFeats()
@@ -96,7 +131,7 @@ function E6_FeatSelectorUI(message)
         if feat == nil then
             _E6Error("Failed to find feat for name: " .. featName)
         else
-            MakeFeatButton(win, windowDimensions[1], message.PlayerId, feat)
+            MakeFeatButton(featUI, windowDimensions[1], { ID = message.PlayerId, Name = message.PlayerName, Abilities = message.Abilities }, feat)
         end
     end
 
