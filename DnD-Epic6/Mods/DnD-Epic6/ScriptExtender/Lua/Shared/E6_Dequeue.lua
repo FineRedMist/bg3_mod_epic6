6ca2d31083e9e63029a161d4299140458402d54e
Brent Scriver <bscriver@oneoddsock.com>
2024-10-13 20:22:21 -0400

    Document Dequeue

--- DnD-Epic6/Mods/DnD-Epic6/ScriptExtender/Lua/Shared/E6_Dequeue.lua
+++ DnD-Epic6/Mods/DnD-Epic6/ScriptExtender/Lua/Shared/E6_Dequeue.lua
@@ -1,11 +1,13 @@
 
---- @class Dequeue
+--- @class Dequeue A double-ended queue.
 --- @field first number The index of first entry in the queue
 --- @field last number The index of the last entry in the queue
 ---A double-ended queue.
 Dequeue = {}
 Dequeue.__index = Dequeue
 
+---Creates a new double-ended queue.
+---@return Dequeue The double ended queue.
 function Dequeue:new()
     local list = setmetatable({}, self)
     list.first = 0
@@ -13,22 +15,30 @@ function Dequeue:new()
     return list
 end
 
+---The number of elements in the queue.
+---@return number The number of elements in the queue.
 function Dequeue:count()
     return self.last - self.first + 1
 end
 
+---Pushes a value onto the left side of the queue.
+---@param value any The value to push onto the left side of the queue.
 function Dequeue:pushleft(value)
     local first = self.first - 1
     self.first = first
     self[first] = value
 end
 
+---Pushes a value onto the right side of the queue.
+---@param value any The value to push onto the right side of the queue.
 function Dequeue:pushright(value)
     local last = self.last + 1
     self.last = last
     self[last] = value
 end
 
+---Pops an entry from the left side of the queue.
+---@return any The value popped from the left side of the queue.
 function Dequeue:popleft()
     local first = self.first
     if first > self.last then
@@ -40,6 +50,8 @@ function Dequeue:popleft()
     return value
 end
 
+---Pops an entry from the right side of the queue.
+---@return any The value popped from the right side of the queue.
 function Dequeue:popright()
     local last = self.last
     if self.first > last then
