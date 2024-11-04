26f313745f992f330472cdd84c31c467400c6405
Brent Scriver <bscriver@oneoddsock.com>
2024-11-03 21:21:57 -0500

    Progress for multiplayer. (#15)

--- DnD-Epic6/Mods/DnD-Epic6/ScriptExtender/Lua/Client/E6_NetClientHandlers.lua
+++ DnD-Epic6/Mods/DnD-Epic6/ScriptExtender/Lua/Client/E6_NetClientHandlers.lua
@@ -1,13 +1,35 @@
 NetClientHandlers = {}
 
+---checks whether the current player is correct for showing/closing UI.
+---@param messageUuid GUIDSTRING The Uuid sent in the message.
+---@return boolean Whether the UUID of the host matches that sent in the message.
+local function CheckPlayerIsRightHandler(messageUuid)
+    local player = GetHost()
+    if player == nil then
+        return false
+    end
+    if not player.Uuid or player.Uuid.EntityUuid ~= messageUuid then
+        return false
+    end
+    return true
+end
+
+
 function NetClientHandlers.ShowFeatSelectorUI(_, payload, peerId)
     ---@type PlayerInformationType
     local message = Ext.Json.Parse(payload)
+
+    if not CheckPlayerIsRightHandler(message.UUID) then
+        return
+    end
 
     E6_FeatSelectorUI(message)
 end
 
 function NetClientHandlers.CloseUI(_, payload, peerId)
+    if not CheckPlayerIsRightHandler(payload) then
+        return
+    end
     E6_CloseUI()
 end
 
