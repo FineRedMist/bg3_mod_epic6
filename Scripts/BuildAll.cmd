df90c3874d2e87b0a20f9e54f0cfc2ad38cd8e38
Brent Scriver <bscriver@oneoddsock.com>
2025-07-08 22:49:17 -0400

    Fix path

--- Scripts/BuildAll.cmd
+++ Scripts/BuildAll.cmd
@@ -2,7 +2,7 @@
 
 for /d %%I in (%~dp0..\LevelOverrides\*) do (
     echo Building %%~nxI...
-    if exist "%%~nxI\Transform.json" (
+    if exist "%%I\Transform.json" (
         call "%~dp0BuildLevel.cmd" %%~nxI
         if errorlevel 1 (
             echo Failed to build %%~nxI
